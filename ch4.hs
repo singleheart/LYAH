fea7abe268061f65c2a9b0969d1450e193c17ce6
singleheart <singleheart@gmail.com>
2017-01-21 13:18:22 +0900

    Chapter 4 section 3 Where!?

--- ch4.hs
+++ ch4.hs
@@ -68,6 +68,34 @@ bmiTell' weight height
     | weight / height ^ 2 <= 30.0 =" You're fat! Lose some weight, fatty!"
     | otherwise                   = "You're a whale, congratulations!"
 
+bmiTell'' :: (RealFloat a) => a -> a -> String
+bmiTell'' weight height
+    | bmi <= 18.5 = "You're underweight, you emo, you!"
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= 30.0 =" You're fat! Lose some weight, fatty!"
+    | otherwise   = "You're a whale, congratulations!"
+    where bmi = weight / height ^ 2
+
+bmiTell''' :: (RealFloat a) => a -> a -> String
+bmiTell''' weight height
+    | bmi <= skinny = "You're underweight, you emo, you!"
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= fat    =" You're fat! Lose some weight, fatty!"
+    | otherwise     = "You're a whale, congratulations!"
+    where bmi = weight / height ^ 2
+          skinny = 18.5
+          normal = 25.0
+          fat    = 30.0
+
+bmiTell'''' :: (RealFloat a) => a -> a -> String
+bmiTell'''' weight height
+    | bmi <= skinny = "You're underweight, you emo, you!"
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= fat    =" You're fat! Lose some weight, fatty!"
+    | otherwise     = "You're a whale, congratulations!"
+    where bmi = weight / height ^ 2
+          (skinny, normal, fat) = (18.5, 25.0, 30.0)
+
 max' :: (Ord a) => a -> a -> a
 max' a b
     | a > b     = a
@@ -78,3 +106,12 @@ a `myCompare` b
     | a > b     = GT
     | a == b    = EQ
     | otherwise = LT
+
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+    where (f:_) = firstname
+          (l:_) = lastname
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+    where bmi weight height = weight / height ^ 2
