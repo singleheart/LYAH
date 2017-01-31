e7123696f45d6d2860c1edbc2c4e1a07130b4ca1
singleheart <singleheart@gmail.com>
2017-01-31 18:13:35 +0900

    Chapter 5 Recursion section 1 Hello recursion!

---
+++ ch5.hs
@@ -0,0 +1,12 @@
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs)
+    | x > maxTail = x
+    | otherwise = maxTail
+    where maxTail = maximum' xs
+
+maximum'' :: (Ord a) => [a] -> a
+maximum'' [] = error "maximum of empty list"
+maximum'' [x] = x
+maximum'' (x:xs) = max x (maximum'' xs)
