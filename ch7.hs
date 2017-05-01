69943adef91428d3293c74265a8e3ccb4d60a016
singleheart <singleheart@gmail.com>
2017-05-02 00:37:30 +0900

    Chapter 7 section 2 Data.List

--- ch7.hs
+++ ch7.hs
@@ -2,3 +2,8 @@ import qualified Data.List as L
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . L.nub
+
+search :: (Eq a) => [a] -> [a] -> Bool
+search needle haystack = 
+    let nlen = length needle
+    in  foldl (\acc x -> if take nlen x == needle then True else acc) False (L.tails haystack)
