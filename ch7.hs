cea2b582af5d1b0571b8f01691d0e12770fd6c62
singleheart <singleheart@gmail.com>
2017-05-06 20:14:36 +0900

    Chapter 7 section 3 Data.Char

--- ch7.hs
+++ ch7.hs
@@ -1,4 +1,5 @@
 import qualified Data.List as L
+import Data.Char
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . L.nub
@@ -7,3 +8,15 @@ search :: (Eq a) => [a] -> [a] -> Bool
 search needle haystack = 
     let nlen = length needle
     in  foldl (\acc x -> if take nlen x == needle then True else acc) False (L.tails haystack)
+
+encode :: Int -> String -> String
+encode shift msg =
+    let ords = map ord msg
+        shifted = map (+ shift) ords
+    in  map chr shifted
+
+encode' :: Int -> String -> String
+encode' shift msg = map (chr . (+ shift) . ord) msg
+
+decode :: Int -> String -> String
+decode shift msg = encode (negate shift) msg
