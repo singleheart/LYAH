bd1a0c1baa9242f555feb9677b938b45997b31d8
singleheart <singleheart@gmail.com>
2017-05-20 19:12:23 +0900

    Chapter 7 section 4 Data.Map

--- ch7.hs
+++ ch7.hs
@@ -1,4 +1,5 @@
 import qualified Data.List as L
+import qualified Data.Map as Map
 import Data.Char
 
 numUniques :: (Eq a) => [a] -> Int
@@ -20,3 +21,37 @@ encode' shift msg = map (chr . (+ shift) . ord) msg
 
 decode :: Int -> String -> String
 decode shift msg = encode (negate shift) msg
+
+phoneBook =   
+    [("betty","555-2938")  
+    ,("betty","342-2492")  
+    ,("bonnie","452-2928")  
+    ,("patsy","493-2928")  
+    ,("patsy","943-2929")  
+    ,("patsy","827-9162")  
+    ,("lucille","205-2928")  
+    ,("wendy","939-8282")  
+    ,("penny","853-2492")  
+    ,("penny","555-2111")  
+    ]  
+
+findKey :: (Eq k) => k -> [(k,v)] -> v
+findKey key xs = snd . head . filter (\(k,v) -> key == k) $ xs
+
+findKey' :: (Eq k) => k -> [(k,v)] -> Maybe v
+findKey' key [] = Nothing
+findKey' key ((k,v):xs) = if key == k
+                            then Just v
+                            else findKey' key xs
+
+findKey'' :: (Eq k) => k -> [(k,v)] -> Maybe v
+findKey'' key = foldr (\(k,v) acc -> if key == k then Just v else acc) Nothing
+
+fromList' :: (Ord k) => [(k,v)] -> Map.Map k v
+fromList' = foldr (\(k,v) acc -> Map.insert k v acc) Map.empty
+
+phoneBookToMap :: (Ord k) => [(k, String)] -> Map.Map k String
+phoneBookToMap xs = Map.fromListWith (\number1 number2 -> number1 ++ ", " ++ number2) xs
+
+phoneBookToMap' :: (Ord k) => [(k, a)] -> Map.Map k [a]
+phoneBookToMap' xs = Map.fromListWith (++) $ map (\(k,v) -> (k,[v])) xs
