c285b8b1472ae9c1364994656e73df60df9f2f15
Kim, Jin <jinn.kim@sk.com>
2017-07-29 16:29:48 +0900

    Chapter 8 section 7 Typeclasses 102

--- ch8.hs
+++ ch8.hs
@@ -98,4 +98,16 @@ treeElem x EmptyTree = False
 treeElem x (Node a left right)  
     | x == a = True  
     | x < a  = treeElem x left  
-    | x > a  = treeElem x right  
+    | x > a  = treeElem x right
+
+data TrafficLight = Red | Yellow | Green
+instance Eq TrafficLight where
+  Red == Red = True
+  Green == Green = True
+  Yellow == Yellow = True
+  _ == _ = False
+
+instance Show TrafficLight where
+  show Red = "Red Light"
+  show Yellow = "Yellow Light"
+  show Green = "Green Light"
