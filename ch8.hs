09263b587bc4808f9a6f202fdfe770395fa18a83
singleheart <singleheart@gmail.com>
2017-08-06 15:21:33 +0900

    Chapter 8 section 8 A yes-no typeclass

--- ch8.hs
+++ ch8.hs
@@ -101,6 +101,7 @@ treeElem x (Node a left right)
     | x > a  = treeElem x right
 
 data TrafficLight = Red | Yellow | Green
+
 instance Eq TrafficLight where
   Red == Red = True
   Green == Green = True
@@ -111,3 +112,32 @@ instance Show TrafficLight where
   show Red = "Red Light"
   show Yellow = "Yellow Light"
   show Green = "Green Light"
+
+class YesNo a where
+    yesno :: a -> Bool
+
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
+
+instance YesNo [a] where
+    yesno [] = False
+    yesno _ = True
+
+instance YesNo Bool where
+    yesno = id
+
+instance YesNo (Maybe a) where
+    yesno (Just _) = True
+    yesno Nothing = False
+
+instance YesNo (Tree a) where
+    yesno EmptyTree = False
+    yesno _ = True
+
+instance YesNo TrafficLight where
+    yesno Red = False
+    yesno _ = True
+
+yesnoIf :: (YesNo y) => y -> a -> a -> a
+yesnoIf yesnoVal yesResult noResult = if yesno yesnoVal then yesResult else noResult
