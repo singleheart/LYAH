c927f54dde9960789051cada67b9455cb0fb1844
singleheart <singleheart@gmail.com>
2017-08-13 22:27:23 +0900

    Chapter 8 section 9 The Functor typeclass

--- ch8.hs
+++ ch8.hs
@@ -141,3 +141,21 @@ instance YesNo TrafficLight where
 
 yesnoIf :: (YesNo y) => y -> a -> a -> a
 yesnoIf yesnoVal yesResult noResult = if yesno yesnoVal then yesResult else noResult
+
+class Functor' f where
+    fmap' :: (a -> b) -> f a -> f b
+
+instance Functor' [] where
+    fmap' = map
+
+instance Functor' Maybe where
+    fmap' f (Just x) = Just (f x)
+    fmap' f Nothing = Nothing
+
+instance Functor' Tree where  
+    fmap' f EmptyTree = EmptyTree  
+    fmap' f (Node x leftsub rightsub) = Node (f x) (fmap' f leftsub) (fmap' f rightsub) 
+
+instance Functor' (Either a) where  
+    fmap' f (Right x) = Right (f x)  
+    fmap' f (Left x) = Left x  
