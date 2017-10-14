4af44f80df8598e6d4df0a98e90fa48c66ad9f6e
Kim, Jin <jinn.kim@sk.com>
2017-10-14 15:52:05 +0900

    add operators

--- solverpn.hs
+++ solverpn.hs
@@ -1,8 +1,12 @@
 import Data.List
 
-solveRPN :: (Num a, Read a) => String -> a
+solveRPN :: String -> Float
 solveRPN = head . foldl foldingFucntion [] . words
   where foldingFucntion (x:y:ys) "*" = (x * y):ys
         foldingFucntion (x:y:ys) "+" = (x + y):ys
         foldingFucntion (x:y:ys) "-" = (y - x):ys
+        foldingFucntion (x:y:ys) "/" = (y / x):ys
+        foldingFucntion (x:y:ys) "^" = (y ** x):ys
+        foldingFucntion (x:xs) "ln" = log x:xs
+        foldingFucntion xs "sum" = [sum xs]
         foldingFucntion xs numberString = read numberString:xs
