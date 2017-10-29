14908aedfd57b95cde591244edcb2695d9089f13
singleheart <singleheart@gmail.com>
2017-10-29 11:57:25 +0900

    using fmap

--- backward.hs
+++ backward.hs
@@ -1,4 +1,3 @@
-main = do line <- getLine
-          let line' = reverse line
-          putStrLn $ "You said " ++ line' ++ " backwards!"
-          putStrLn $ "Yes, you really said " ++ line' ++ " backwards!"
+main = do line <- fmap reverse getLine
+          putStrLn $ "You said " ++ line ++ " backwards!"
+          putStrLn $ "Yes, you really said " ++ line ++ " backwards!"
