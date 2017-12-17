b3cc686373c0e39d8d2fb4813c547261171eb717
singleheart <singleheart@gmail.com>
2017-12-17 17:09:27 +0900

    add routine function

--- pole.hs
+++ pole.hs
@@ -15,3 +15,12 @@ x -: f = f x
 
 banana :: Pole -> Maybe Pole
 banana _ = Nothing
+
+routine :: Maybe Pole
+routine = case landLeft 1 (0, 0) of
+    Nothing -> Nothing
+    Just pole1 -> case landRight 4 pole1 of
+        Nothing -> Nothing
+        Just pole2 -> case landLeft 2 pole2 of
+            Nothing -> Nothing
+            Just pole3 -> landLeft 1 pole3
