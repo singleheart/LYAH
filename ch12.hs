69576eb7839a9f2c781e73739fcd02b58cb92828
singleheart <singleheart@gmail.com>
2017-12-17 17:40:58 +0900

    Chapter 12 section 4 do notation

--- ch12.hs
+++ ch12.hs
@@ -3,3 +3,8 @@ foo = do
     x <- Just 3
     y <- Just "!"
     Just (show x ++ y)
+
+wopwop :: Maybe Char
+wopwop = do
+    (x:xs) <- Just ""
+    return x
---
+++ marySue.hs
@@ -0,0 +1,4 @@
+marySue :: Maybe Bool
+marySue = do
+    x <- Just 9
+    Just (x > 8)
