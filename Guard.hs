4d7976bccdb654084493a40d20d26fdd47b34eb5
singleheart <singleheart@gmail.com>
2017-12-23 16:13:40 +0900

    Chapter 12 section 5 The list monad

---
+++ Guard.hs
@@ -0,0 +1,15 @@
+module Guard
+(guard
+) where
+
+class Monad m => MonadPlus m where
+    mzero :: m a
+    mplus :: m a -> m a -> m a
+
+instance MonadPlus [] where
+    mzero = []
+    mplus = (++)
+
+guard :: (MonadPlus m) => Bool -> m ()
+guard True = return ()
+guard False = mzero
