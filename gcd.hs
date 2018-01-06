9bcd926c89cebb934f8b398098c4b397cd9bd640
singleheart <singleheart@gmail.com>
2018-01-06 23:47:46 +0900

    Difference lists

--- gcd.hs
+++ gcd.hs
@@ -1,4 +1,5 @@
 import Control.Monad.Writer
+import DiffList
 
 gcd' :: Int -> Int -> Writer [String] Int
 gcd' a b
@@ -9,12 +10,12 @@ gcd' a b
         tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
         gcd' b (a `mod` b)
 
-gcdReverse :: Int -> Int -> Writer [String] Int
+gcdReverse :: Int -> Int -> Writer (DiffList String) Int
 gcdReverse a b
     | b == 0    = do
-        tell ["Finished with " ++ show a]
+        tell (toDiffList ["Finished with " ++ show a])
         return a
     | otherwise = do
         result <- gcdReverse b (a `mod` b)
-        tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+        tell (toDiffList [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)])
         return result
