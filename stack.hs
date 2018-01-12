cf92d3c9394d20a9f5148c24bb575f2a5df24386
singleheart <singleheart@gmail.com>
2018-01-13 00:15:09 +0900

    The State monad

--- stack.hs
+++ stack.hs
@@ -1,13 +1,38 @@
+import Control.Monad.State
+
 type Stack = [Int]
 
-pop :: Stack -> (Int, Stack)
-pop (x:xs) = (x, xs)
+pop :: State Stack Int
+pop = state $ \(x:xs) -> (x, xs)
 
-push :: Int -> Stack -> ((), Stack)
-push a xs = ((), a:xs)
+push :: Int -> State Stack ()
+push a = state $ \xs -> ((), a:xs)
 
-stackManip :: Stack -> (Int, Stack)
-stackManip stack = let
-    ((), newStack1) = push 3 stack
-    (a , newStack2) = pop newStack1
-    in pop newStack2
+stackManip :: State Stack Int
+stackManip = do
+    push 3
+    pop
+    pop
+
+stackStuff :: State Stack ()
+stackStuff = do
+    a <- pop
+    if a == 5
+        then push 5
+        else do
+            push 3
+            push 8
+
+moreStack :: State Stack ()
+moreStack = do
+    a <- stackManip
+    if a == 100
+        then stackStuff
+        else return ()
+
+stackyStack :: State Stack ()
+stackyStack = do
+    stackNow <- get
+    if stackNow == [1,2,3]
+        then put [8,3,1]
+        else put [9,2,1]
