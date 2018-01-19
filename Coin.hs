9d7cc656a7dbae95377bd65463a9be9315290e1c
singleheart <singleheart@gmail.com>
2018-01-19 11:52:38 +0900

    Chapter 13 section 6 Making monads

---
+++ Coin.hs
@@ -0,0 +1,13 @@
+module Coin
+where
+
+import Data.Ratio
+import Prob
+
+data Coin = Heads | Tails deriving (Show, Eq)
+
+coin :: Prob Coin
+coin = Prob [(Heads,1%2),(Tails,1%2)]
+
+loadedCoin :: Prob Coin
+loadedCoin = Prob [(Heads,1%10),(Tails,9%10)]
